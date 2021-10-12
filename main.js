107167dcf4e3df93ed3c674edf5105cb917d1a6e
Shikanto <antonello.anzini.comunicazioni@gmail.com>
2021-10-12 18:01:43 +0200

    esercizio fatto ma non sono soddisfatto

--- main.js
+++ main.js
@@ -10,13 +10,36 @@ for (let i = 1; i <= 100; i++){
     const fizz = i % 3 === 0;
     const buzz = i % 5 === 0;
     const fizzBuzz = (i % 3 === 0) && (i % 5 === 0);
+    let alertFizzBuzz = ""
+
+    let bgColor= "";
+    let numero = i;
+
+    if (fizzBuzz) { 
+        bgColor = "bg-fizzbuzz";
+        alertFizzBuzz = "fizzbuzz";
+        numero="";
+    } else if (fizz) {
+        bgColor = "bg-fizz";
+        alertFizzBuzz = "fizz";
+        numero="";
+    } else if (buzz) {
+        bgColor = "bg-buzz";
+        alertFizzBuzz = "buzz";
+        numero="";
+    } else {
+        bgColor = "bg-normal";
+    }
+    
 
     row.innerHTML +=    `<div class="col-7 p-0 justify-content-center align-items-center">
-                                <div class="box d-flex justify-content-center align-items-center">
-                                    <div> ${i} </div>
+                                <div class="box d-flex justify-content-center align-items-center ${bgColor}">
+                                    <div> ${numero} ${alertFizzBuzz} </div>
                                 </div>
                             </div>`;
-        
+
+    
+
     containerFizzBuzz.append(row);
 }
 
