584972bb3e709d140c00baaf24c0fc07555d8528
Shikanto <antonello.anzini.comunicazioni@gmail.com>
2021-10-12 17:21:26 +0200

    square created

---
+++ main.js
@@ -0,0 +1,22 @@
+const containerFizzBuzz = document.getElementsByClassName("container")[1];
+/* console.log(containerFizzBuzz); */
+
+
+
+const row = document.createElement("div");
+row.classList.add("row");
+
+for (let i = 1; i <= 100; i++){
+    const fizz = i % 3 === 0;
+    const buzz = i % 5 === 0;
+    const fizzBuzz = (i % 3 === 0) && (i % 5 === 0);
+
+    row.innerHTML +=    `<div class="col-7">
+                                <div class="box d-flex justify-content-center align-items-center">
+                                    <div> ${i} </div>
+                                </div>
+                            </div>`;
+        
+    containerFizzBuzz.append(row);
+}
+
