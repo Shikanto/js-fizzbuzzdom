618e00d70723bf31b60ec25e41eea187d33ccd94
Shikanto <antonello.anzini.comunicazioni@gmail.com>
2021-10-13 10:30:03 +0200

    corrections

--- main.js
+++ main.js
@@ -4,39 +4,39 @@ const containerFizzBuzz = document.getElementsByClassName("container")[1];
 
 
 const row = document.createElement("div");
-row.classList.add("row");
+row.classList.add("row", "gap-5");
 
 for (let i = 1; i <= 100; i++){
     const fizz = i % 3 === 0;
     const buzz = i % 5 === 0;
     const fizzBuzz = (i % 3 === 0) && (i % 5 === 0);
     
-    let alertFizzBuzz = ""
+    /* let alertFizzBuzz = "" */
     let bgColor= "";
     let numero = i;
 
     if (fizzBuzz) { 
         bgColor = "bg-fizzbuzz";
-        alertFizzBuzz = "fizzbuzz";
-        numero="";
+        /* alertFizzBuzz = "fizzbuzz"; */
+        numero="fizzbuzz";
     } else if (fizz) {
         bgColor = "bg-fizz";
-        alertFizzBuzz = "fizz";
-        numero="";
+        /* alertFizzBuzz = "fizz"; */
+        numero="fizz";
     } else if (buzz) {
         bgColor = "bg-buzz";
-        alertFizzBuzz = "buzz";
-        numero="";
+        /* alertFizzBuzz = "buzz"; */
+        numero="buzz";
     } else {
         bgColor = "bg-normal";
     }
     
 
     row.innerHTML +=    `<div class="col-7 p-0">
-                                <div class="box d-flex justify-content-center align-items-center ${bgColor}">
-                                    <div> ${numero} ${alertFizzBuzz} </div>
-                                </div>
-                            </div>`;
+                            <div class="box d-flex justify-content-center align-items-center ${bgColor}">
+                                <div> ${numero} </div>
+                            </div>
+                        </div>` ;
 
     
 
